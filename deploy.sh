1cbe1eb39bb498e3c2ffc5fdc5ca4c3b630dc2fe
Mithun Technologies <kolicheramkalpana@gmail.com>
2022-06-13 22:01:49 +0530

    updated in dev

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 updating this file
 updating in master branch
+This line is upadating in dev branch
 This line we can use for testing
 Again upadated this file
 This file contains the deployment script
